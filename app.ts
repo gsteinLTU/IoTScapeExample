4886cf3a9963522f74020bdca362a86a73c0b686
Gordon Stein <gstein@ltu.edu>
2021-01-07 14:32:35 -0600

    Change to announce on interval

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 config();
 
-import { add, hello, ping, timer, definition } from "./rpcs";
+import { definition } from "./rpcs";
 import { Server } from "./server";
 
 const server = new Server();
@@ -9,4 +9,6 @@ const server = new Server();
 const host = process.env.METASCAPE_SERVER ?? "localhost";
 const port = Number.parseInt(process.env.METASCAPE_PORT ?? "1975");
 
-server.announce(host, port, JSON.stringify(definition));
+setInterval(() => {
+    server.announce(host, port, JSON.stringify(definition));
+}, 5000);
